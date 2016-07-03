f0b66bfd23abc3abc052b5176a606701f0aa342b
eddddz <ef.vanwyk@gmail.com>
2016-07-03 19:01:11 +0200

    Submission of Project 1 - Blinking an LED

---
+++ Project1_Blinking_An_LED/BlinkingLED/BlinkingLED.ino
@@ -0,0 +1,21 @@
+/*
+ * Tutorial Project 1 - Blinking LED
+ */
+
+int ledPin = 13;
+
+void setup() {
+  //initialize pins as outputs
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // power ledPin on
+  digitalWrite(ledPin, HIGH);
+  // wait 1 second
+  delay(1000);  
+  // power ledPin off
+  digitalWrite(ledPin, LOW);
+  // wait 1 second
+  delay(1000);
+}
